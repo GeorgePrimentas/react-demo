157651bfd639e0383ac1aa310257744e6eea3eb6
George <108883931+GeorgePrimentas@users.noreply.github.com>
2023-06-22 12:56:40 +0100

    created Article component + css

---
+++ src/Article/Article.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './Article.css'
+
+const Article = () => <article className="special-article">
+<h2>Vincent van Gogh</h2>
+<p>Dutch painter, 1853-1890</p>
+</article>
+
+export default Article;
