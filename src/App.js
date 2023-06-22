d77cfac07817f8eb765f985b91b1d3b9b60d6a24
Andriana <andrianasaffo@gmail.com>
2023-06-22 12:08:05 +0100

    remove component from app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import "./App.css";
 import Header from "./Header";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
