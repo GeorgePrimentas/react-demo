74225285ffe0860372b8797764c1c2cb6c79768e
Gayle Ngozi <gaylengozi@gmail.com>
2023-06-22 13:32:42 +0100

    footer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Footer from './Footer';
+
 import './Footer.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Footer/>
+     
       </header>
     </div>
   );
